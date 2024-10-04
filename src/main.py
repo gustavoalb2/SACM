3948afa548d8fbab8727046809d6b735a0adb306
gustavoalb <85568069+gustavoalb2@users.noreply.github.com>
2024-10-04 12:38:32 -0500

    função para mudar tema

--- src/main.py
+++ src/main.py
@@ -1,6 +1,55 @@
 import ttkbootstrap as ttk
-import tkinter as tk
 from ttkbootstrap.constants import *
+from ttkbootstrap import Style
+# import sqlite3
+
+# Conectando ao banco de dados
+# conn = sqlite3.connect('database\consultas.db')
+# cursor = conn.cursor()
+
+# Criando a tabela de pacientes
+# cursor.execute('''
+# CREATE TABLE IF NOT EXISTS pacientes (
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     nome TEXT NOT NULL,
+#     data_nascimento TEXT,
+#     cpf TEXT,
+#     telefone TEXT,
+#     email TEXT,
+#     endereco TEXT
+# )
+# ''')
+
+# Inserindo um paciente
+# cursor.execute('''
+# INSERT INTO pacientes (nome, data_nascimento, cpf, telefone, email, endereco)
+# VALUES (?, ?, ?, ?, ?, ?)
+# ''', ("Maria Oliveira", "1985-03-10", "98765432100", "999887766", "maria@example.com", "Rua B, 456"))
+
+# Salvando as mudanças
+# conn.commit()
+
+# Consultando todos os pacientes
+# cursor.execute('SELECT * FROM pacientes')
+# pacientes = cursor.fetchall()
+
+# Exibindo os pacientes
+# for paciente in pacientes:
+#     print(paciente)
+
+# Atualizando o telefone de um paciente
+# cursor.execute('''
+# UPDATE pacientes
+# SET telefone = ?
+# WHERE cpf = ?
+# ''', ("999665544", "98765432100"))
+
+# Salvando as mudanças
+# conn.commit()
+
+# Fechando a conexão
+# conn.close()
+
 
 class Tela:
     def __init__(self, master):
@@ -31,16 +80,16 @@ class Tela:
         self.mnu_agendamentos.add_separator()
         self.mnu_agendamentos.add_command(label='Cadastrar agendamentos')
         
-        self.mnu_config.add_command(label='Temas')
+        self.mnu_config.add_command(label='Temas', command=self.mudar_tema)
 
         self.janela.config(menu=self.mnu_barra)
 
     def listar_pacientes(self):
-        self.top_pacientes = ttk.Toplevel(self.janela)
-        self.top_pacientes.title('Listar Pacientes')
-        self.top_pacientes.geometry('900x800')
+        self.limpar_tela()
+        self.janela.title('Listar Pacientes')
+        self.janela.geometry('900x800')
 
-        self.frm = ttk.Frame(self.top_pacientes)
+        self.frm = ttk.Frame(self.janela)
         self.frm.pack()
         
         colunas = ['ID', 'Nome', 'CPF', 'Telefone', 'Email', 'Endereco']
@@ -67,11 +116,11 @@ class Tela:
         self.btn_voltar.grid(row=1, column=0, ipadx=80)
     
     def cadastrar_paciente(self):
-        self.top_cadastrar_paciente = ttk.Toplevel(self.janela)
-        self.top_cadastrar_paciente.title('Cadastrar Paciente')
-        self.top_cadastrar_paciente.geometry('900x800')
+        self.limpar_tela()
+        self.janela.title('Cadastrar Paciente')
+        self.janela.geometry('900x800')
         
-        self.frm = ttk.Frame(self.top_cadastrar_paciente)
+        self.frm = ttk.Frame(self.janela)
         self.frm.pack()
         
         self.label_nome = ttk.Label(self.frm, text='Nome:')
@@ -96,7 +145,7 @@ class Tela:
         
         self.label_endereco = ttk.Label(self.frm, text='Endereco:')
         self.label_endereco.grid(row=4, column=0, padx=10, pady=10)
-        self.txt_endereco = ttk.Text(self.frm, wrap=tk.WORD, height=2, width=50)
+        self.txt_endereco = ttk.Text(self.frm, wrap=ttk.WORD, height=2, width=50)
         self.txt_endereco.grid(row=4, column=1)
         
         self.btn_salvar = ttk.Button(self.frm, text='Salvar', style='success')
@@ -105,6 +154,37 @@ class Tela:
         self.bnt_voltar = ttk.Button(self.frm, text='Voltar', command=self.voltar, style='info')
         self.bnt_voltar.grid(row=6, column=0, columnspan=2, padx=10, ipadx=80)
         
+    def mudar_tema(self):
+        self.top_tema = ttk.Toplevel(self.janela)
+        self.top_tema.title('Temas')
+        self.top_tema.geometry('900x200')
+        self.style = Style() 
+        
+        #frame com todos os temas
+        self.frm_tema = ttk.LabelFrame(self.top_tema, text="Temas do TTk BootStrap")
+        self.frm_tema.pack()
+        
+        self.tema_selecionado = ttk.StringVar(value=self.style.theme_use())
+        
+        #radio button para escolher o tema, todos os temas
+        colunas_por_linha = 8
+        linha = 0
+        coluna = 0
+        for tema in self.style.theme_names():
+            rb = ttk.Radiobutton(self.frm_tema, text=tema, value=tema, variable=self.tema_selecionado, command=self.change_tema)
+            rb.grid(row=linha, column=coluna, padx=3)
+            coluna += 1
+            if coluna > colunas_por_linha:
+                coluna = 0
+                linha += 1   
+        
+        self.btnvoltar = ttk.Button(self.top_tema, text='Voltar', command=self.voltar, style='info')
+        self.btnvoltar.pack()
+    
+    def change_tema(self):
+        tema_selecionado = self.tema_selecionado.get()
+        self.style.theme_use(tema_selecionado)
+
     def limpar_tela(self):
         for widget in self.janela.winfo_children():
             widget.destroy()
